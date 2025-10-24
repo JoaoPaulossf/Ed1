9bc5e2254bae24c042705245fc574831ae74faec
Joao Paulo <linkmagnofxd@gmail.com>
2025-10-24 00:35:34 -0300

    Finalizado e comentado

--- miniEP2/mEP2.c
+++ miniEP2/mEP2.c
@@ -3,50 +3,28 @@
 
 typedef long long ll;
 
-//Primeira tentativa, usando loops aninhados
+//função que recebe um vetor ordenado e verifica se alguma soma entre dois elementos é igual ao valor fornecido
+void procurarSoma(ll *vetor,ll somaProcurada, unsigned int tamanhoVetor){
+    int indiceEsquerda = 0;
+    int indiceDireita = tamanhoVetor - 1;
 
-// void procurarSoma(ll *numeroDasCasas,ll somaProcurada,unsigned int quantidadeCasas){
-//     ll somaAtual = 0;
-//     unsigned int ultimoIndice = quantidadeCasas - 1;
-//     for(int i = 0; i < quantidadeCasas; i++){
-//         for(int j = ultimoIndice - (i + 1); j < quantidadeCasas; j++){
-//             somaAtual = numeroDasCasas[i] + numeroDasCasas[j];
-
-//             if(somaAtual == somaProcurada){
-
-//                 printf("SIM\n%d %d\n",i+1,j+1);
-
-//                 return;
-//             }
-//         }
-//     }
-//     printf("NAO\n");
-
-// }
-
-// Segunda tentativa, usando a ideia de dois ponteiros, sabendo que o vetor esta ordenado
-void procurarSoma(ll *vetor,ll somaProcurada, unsigned int tamanho){
-    int esquerda = 0;
-    int direita = tamanho - 1;
-    while(esquerda < direita){
-        ll soma = vetor[esquerda] + vetor[direita];
+    
+    while(indiceEsquerda < indiceDireita){
+        ll soma = vetor[indiceEsquerda] + vetor[indiceDireita];
+        
         if (soma == somaProcurada){
-            printf("SIM\n%d %d\n",esquerda + 1,direita + 1);
+            printf("SIM\n%d %d\n",indiceEsquerda + 1,indiceDireita + 1);
             return;
         }
-        if (soma < somaProcurada){
-            esquerda++;
-        }
-        if(soma > somaProcurada){
-            direita--;
-        }
+        else if (soma < somaProcurada) indiceEsquerda++;
+        else if(soma > somaProcurada) indiceDireita--;
     }
     printf("NAO\n");
 }
 
 int main(){
-    // para atender a restrição de (2 <= N <= 10 ^ 5)
-    unsigned int quantidadeCasas = 0;
+    unsigned int quantidadeCasas = 0; // para atender a restrição de (2 <= N <= 10 ^ 5)
+    // -- entradas --
     scanf("%d",& quantidadeCasas);
 
     ll *numeroDasCasas = NULL;
@@ -64,9 +42,10 @@ int main(){
     ll somaProcurada;
     scanf("%lld",&somaProcurada);
 
-    procurarSoma(numeroDasCasas,somaProcurada,quantidadeCasas);
+    // -- saidas --
+    procurarSoma(numeroDasCasas,somaProcurada,quantidadeCasas); //chamada da função principal para a resolução do problema
     
-    free(numeroDasCasas);
+    free(numeroDasCasas); // desalocando a mémoria
 
     return 0;
 }
